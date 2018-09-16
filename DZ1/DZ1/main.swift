5b3aecb86419f6b31d5ad1bc417deac09e258bd2
Kostya0955 <3711733@gmail.com>
2018-09-16 12:42:06 +0300

    Первый коммит

---
+++ DZ1/DZ1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DZ1
+//
+//  Created by Konstantin on 16.09.2018.
+//  Copyright © 2018 Konstantin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
