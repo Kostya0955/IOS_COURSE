1615eb027f1515a70edf25414d4b4f54b3960e19
Kostya0955 <3711733@gmail.com>
2018-10-18 02:29:16 +0300

    что-то с прогой

---
+++ DZ2/gallery_test/SecondScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondScreenViewController.swift
+//  gallery_test
+//
+//  Created by Головнёв Константин on 17.10.2018.
+//  Copyright © 2018 Головнёв Константин. All rights reserved.
+//
+
+import UIKit
+
+class SecondScreenViewController: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ DZ2/gallery_test/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  gallery_test
+//
+//  Created by Konstantin on 23.09.2018.
+//  Copyright © 2018 Konstantin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   @IBAction func ShowMessage(){
+        print ("Hello Button") 
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
