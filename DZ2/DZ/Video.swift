32171d043da9cb9eb84a6f71034d82072a405665
Kostya0955 <3711733@gmail.com>
2018-10-18 23:06:01 +0300

    вот уже точно финальный проект

---
+++ DZ2/DZ/Video.swift
@@ -0,0 +1,24 @@
+//
+//  Good.swift
+//   gallery_test
+//  Created by Головнёв Константин on 17.10.2018.
+//  Copyright © 2018 Головнёв Константин. All rights reserved.
+//
+
+import UIKit
+
+final class Video {
+    var id: Int
+    var image: UIImage
+    var name: String
+    var descr: String
+    var views: Int
+    
+    init(id: Int, image: UIImage, name: String, descr: String, views: Int) {
+        self.id = id
+        self.image = image
+        self.name = name
+        self.descr = descr
+        self.views = views
+    }
+}
